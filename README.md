b5bc8a14b59e63478fe511899dcd300470dbbc04
Anashm24 <anashoumaid19@gmail.com>
2023-11-24 18:29:38 +0100

    my project

---
+++ README.md
@@ -0,0 +1,1 @@
+simple calculator with C using switch cases. This calculator takes two operands and an arithmetic operator (+, -, *, /)
---
+++ calculator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+int main(int argc, char *argv[])
+{
+  float v_One;
+  float v_Two;
+  char operator;
+  float result;
+
+  printf("Enter calculation:\n\n");
+  scanf("%f %c %f", &v_One, &operator, & v_Two);
+
+  switch(operator)
+    {
+    case '/': result = v_One/v_Two;
+      break;
+    case '*': result = v_One*v_Two;
+      break;
+    case '+': result = v_One+v_Two;
+      break;
+    case '-': result = v_One-v_Two;
+      break;
+    case '^': result = pow(v_One,v_Two);
+      break;
+    case ' ': result = sqrt(v_Two);
+      break;
+    default: goto fail;
+    }
+  printf("%.9g%c%.9g =  %.6g\n\n",v_One,operator, v_Two, result);
+  goto exit;
+ fail:
+  printf("Fail.\n");
+ exit:
+  return 0;
+}
